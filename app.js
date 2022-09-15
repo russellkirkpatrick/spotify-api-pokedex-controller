dd49a554a6a71c880dd3a17c0e688cd59d86ce75
Russell Kirkpatrick <109391715+russellkirkpatrick@users.noreply.github.com>
2022-09-15 00:42:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ function getAccessToken(){
 
 
 function getApiReq(){
-  fetch('https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=50&offset=0', {
+  fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=50&offset=0', {
     method: 'GET', headers: {
         'Authorization': 'Bearer '  + access_token 
     }
